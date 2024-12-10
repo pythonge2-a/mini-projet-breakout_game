02b8131bac3d2a35561c09e8f7e0df1fc9284c31
tornare-magali <145576863+tornare-magali@users.noreply.github.com>
2024-12-10 10:02:48 +0100

    Ajout d'une bordure sur les briques

--- breakout/brick_field.py
+++ breakout/brick_field.py
@@ -14,15 +14,17 @@ class Brick:
         self.position = position
         self.lives = lives
         self.size = [C.BRICK_WIDTH, C.BRICK_HEIGHT]
-
         self.color = C.BRICK_COLOR_MAP[self.lives]
+        self.brick_border_width = C.BRICK_BORDER_WIDTH
         pass
 
     def show(self):
         """Commentaire de fonction"""
         rect_position = (self.position, self.size)
+    
         # Draws a rectangle to the screen
         pygame.draw.rect(self.screen, self.color, rect_position)
+        pygame.draw.rect(self.screen, C.BRICK_BORDER_COLOR, rect_position, width = self.brick_border_width)
 
 
 class Brick_field:
@@ -38,7 +40,7 @@ class Brick_field:
             for j in range(C.BRICK_NB_BRICKS_X):
                 # Compute brick position
                 x = j * (C.BRICK_WIDTH + C.BRICK_HORIZONTAL_SPACING) 
-                y = i * (C.BRICK_HEIGHT + C.BRICK_VERTICAL_SPACING)
+                y = C.BRICK_TOP_CLEARANCE + i * (C.BRICK_HEIGHT + C.BRICK_VERTICAL_SPACING)
                 pos = [x, y]
                 lives = np.random.randint(0, C.BRICK_MAX_LIVES)
                 # Add brick
