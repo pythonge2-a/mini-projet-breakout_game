12d5be659f75721c429c3c3d1450a2503363d971
Bastien Chaignat <root@BC-3604>
2024-12-10 13:44:02 +0100

    add function for each bonus

--- breakout/bonus.py
+++ breakout/bonus.py
@@ -14,7 +14,7 @@ class bolus:
         "add_ball",
         "unstoppable",
         "glu",
-        "break",
+        "break_brick",
         "net",
     ]
     list_malus = [
@@ -31,24 +31,27 @@ class bolus:
     proba_bonus = [100, 50, 75, 75, 100, 10, 25, 50, 10]
     proba_malus = [100, 75, 75, 50, 75, 20, 25, 10, 5]
 
-    def __init__(self, speed=C.BONUS_SPEED, y=None, x=None):
+    def __init__(self, speed=C.BONUS_SPEED, y=None, x=None, brick = None, bonus = False, malus = False):
         self.speed = speed
         self.y = y
         self.x = x
+        self.brick = brick
+        self.bonus = bonus
+        self.malus = malus
 
-    def add_bonus(self):
+    def set_bonus(self):
         """Créé un bonus aléatoire"""
 
         bonus = rd.choices(self.list_bonus, self.proba_bonus)
         return bonus
 
-    def add_malus(self):
+    def set_malus(self):
         """Créé un malus aléatoire"""
 
         malus = rd.choices(self.list_malus, self.proba_malus)
         return malus
 
-    def add_bolus(self):
+    def set_bolus(self):
         """Créé un malus ou un bonus aléatoire"""
 
         bolus = rd.choices(
@@ -67,3 +70,77 @@ class bolus:
     def kill(self):
         """Détruit le bonus/malus"""
         del self
+
+    def grow_racket(self) :
+        """Bonus d'agrandissement de la raquette"""
+        pass
+
+    def grow_ball(self) :
+        """"Bonus d'agrandissement de la balle"""
+        pass
+
+    def speed_up_racket(self) :
+        """Bonus d'accélération de la raquette"""
+        pass
+
+    def speed_down_ball(self) :
+        """Bonus de ralentissement de la balle"""
+        pass
+
+    def add_ball(self) :
+        """Bonus d'ajout d'une balle"""
+        pass
+
+    def unstoppable(self) :
+        """Bonus pour que la balle détruise tout sur son passage"""
+        pass
+
+    def glu(self) :
+        """Bonus qui arrête la balle sur la raquette (lâche avec un bouton ou un temps)"""
+        pass
+
+    def break_brick(self) :
+        """Bonus qui casse une brique aléatoire"""
+        pass
+
+    def net(self) :
+        """Bonus qui empêche de perdre une bille"""
+        pass
+
+    def speed_up_ball(self) :
+        """Malus d'accélération de la balle"""
+        pass
+
+    def speed_down_racket(self) :
+        """Malus de ralentissement de la raquette"""
+        pass
+
+    def shrink_racket(self) :
+        """Malus de rétrécissement de la raquette"""
+        pass
+
+    def shrink_ball(self) :
+        """Malus de rétrécissement de la balle"""
+        pass
+
+    def reinforce_brick(self) :
+        """Malus d'ajout d'une vie à une brique"""
+        pass
+
+    def ghost(self) :
+        """Malus qui empêche la balle de toucher les briques"""
+        pass
+
+    def reverse(self) :
+        """Malus qui change les sens des contrôles"""
+        pass
+
+    def explosion(self) :
+        """Malus qui rend des briques ou la balle explosive (vitesse de la bille augmente, angle change)"""
+        pass
+
+    def unbreakable(self) :
+        """Malus qui rend une brique temporairement incassable"""
+        pass
+    
+
