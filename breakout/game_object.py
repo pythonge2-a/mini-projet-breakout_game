9d7f1b75bec3c1f940bdabe575774bb2bffacc6a
Ding-Jeremy <145600655+Ding-Jeremy@users.noreply.github.com>
2024-12-13 09:53:35 +0100

    Add game_object, init_py

---
+++ breakout/game_object.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pygame
+
+
+class Game_object:
+    """Defines an object that is shown to the screen.
+    It has everything that said object MUST contain.
+    Nothing Less, nothing more.
+    """
+
+    def __init__(
+        self,
+        position: np.array,
+        size: np.array,
+        sprites: list,
+        velocity: np.array = np.array([0, 0]),
+        acceleration: np.array = np.array([0, 0]),
+    ):
+        # Defines physical properties
+        self.position = position
+        self.size = size
+        self.velocity = velocity
+        self.acceleration = acceleration
+
+        # Defines graphical properties (sprites)
+        # Defines the different sprites for this object (useful to animate said object)
+        self.sprites = sprites
+        self.current_sprite = self.sprites[0]
+        # Defines current sprite to show
+        self.animation_index = 0
+        self.animation_range = len(self.sprites)
+
+    def update(self):
+        """Updates object, it's animation, it's position"""
+        self.velocity += self.acceleration
+        self.position += self.velocity
+
+    def update_animation(self):
+        """Udpate sprite"""
+        self.animation_index += 1
+        # Check animation index
+        if self.animation_index == self.animation_range:
+            self.animation_index = 0
+        # Modify sprite
+        self.current_sprite = self.sprites[self.animation_index]
